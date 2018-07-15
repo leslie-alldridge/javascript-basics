eae2fc28d091dc41a14d5d0d904b634f74f2cab3
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-07-16 10:24:12 +1200

    completed first challenge

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+//console.log("hello world");
+
+// first challenge 
+
+var markHeight = 70;
+var markWeight = 100;
+
+var johnHeight = 90;
+var johnWeight = 100;
+
+var highestBMI = false;
+
+//math
+
+var markBMI = markWeight / (markHeight * 2);
+var johnBMI = johnWeight / (johnHeight * 2);
+
+highestBMI = markBMI >= johnBMI;
+console.log("Is Mark's BMI higher than John's?" + ' ' + highestBMI);
+
+if(markBMI >= johnBMI) {
+    console.log("Mark's BMI is higher than John's");
+} else {
+    console.log("John's BMI is higher than Mark's");
+}
