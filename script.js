6825423489a9ef4ed051e1e07f64ac1cab2cd119
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-16 10:46:44 +1200

    switch and ternary

--- script.js
+++ script.js
@@ -23,3 +23,57 @@ if(markBMI >= johnBMI) {
 } else {
     console.log("John's BMI is higher than Mark's");
 }
+
+//boolean logic
+
+var firstName = 'Leslie';
+var age = 26;
+
+if (age < 13) {
+    console.log(firstName + ' ' + 'is a boy');
+} else if (age >= 13 && age <20) {
+    console.log(firstName + ' ' + 'is a teenager');
+} else if (age >= 20 && age < 30){
+    console.log(firstName + ' ' + 'is a young man');
+} else {
+    console.log(firstName + ' ' + 'is a man');
+}
+
+age >= 18 ? console.log(firstName + ' ' + 'drinks beer') : console.log(firstName + ' ' + 'drinks juice');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+
+console.log("You must drink " + drink);
+
+//switch statement
+
+var job = 'sensei';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+    case 'sensei':
+        console.log(firstName + ' teaches people');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives people');
+        break;
+    case 'gamer':
+        console.log(firstName + ' plays with people');   
+        break;
+    default: 
+        console.log(firstName + ' has no idea what they\'re doing'); 
+}
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' ' + 'is a boy');
+        break;
+    case age >= 13 && age <20:
+        console.log(firstName + ' ' + 'is a teenager');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' ' + 'is a young man');
+        break;
+    default:
+        console.log(firstName + ' ' + 'is a man');
+}
